8459da8880b48b59d31bf9065c218cd36a072829
Torbjorn van Heeswijck <t.vanheeswijck@gmail.com>
2016-03-16 23:20:56 +1030

    Pan controls
    Pan screen with single touch or right-click.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
     public Camera thisCamera;
-	public Vector3 cameraOffset;
-	public float moveTime, zoomSpeed, zoomTime, zoomDelay, targetSize;
+	public Vector3 cameraOffset, lastPosition;
+	public float zoomSpeed, zoomRatio, panSpeed, panRatio, lastTouch, returnDelay, targetSize;
 
-	public IEnumerator FocusCamera(Transform parentTransform){
+#if UNITY_EDITOR || UNITY_STANDALONE
+    private bool firstPan;
+#endif
+
+    public IEnumerator FocusCamera(Transform parentTransform){
 
 		transform.SetParent (parentTransform);
 
@@ -20,7 +24,7 @@ public class CameraScript : MonoBehaviour {
             while (Game.Instance.state == Game.State.MENU)
                 yield return new WaitForSeconds(0.1f);
 
-            Vector3 newPosition = Vector3.MoveTowards (transform.position, target, Time.deltaTime / moveTime);
+            Vector3 newPosition = Vector3.MoveTowards (transform.position, target, Time.deltaTime * panSpeed);
 
 			transform.position = newPosition;
 
@@ -34,15 +38,27 @@ public class CameraScript : MonoBehaviour {
         transform.SetParent(player);
         thisCamera.orthographicSize = 10f;
         targetSize = 5f;
+#if UNITY_EDITOR || UNITY_STANDALONE
+        firstPan = true;
+#endif
     }
 
     void Update() {
+        // Camera pan controlling code
 #if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            lastTouch = Time.time;
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * panRatio * panSpeed, -touchDeltaPosition.y * panRatio * panSpeed, 0);
+        }          
+
+        // Camera zoom controlling code
         // If there are two touches on the device...
         if (Input.touchCount == 2)
         {
             // Store the last time zoom was requested
-            zoomTime = Time.time;
+            lastTouch = Time.time;
 
             // Store both touches.
             Touch touchZero = Input.GetTouch(0);
@@ -60,34 +76,53 @@ public class CameraScript : MonoBehaviour {
             float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
             // change the orthographic size based on the change in distance between the touches.
-            thisCamera.orthographicSize += deltaMagnitudeDiff * zoomSpeed;
+            thisCamera.orthographicSize += zoomRatio * deltaMagnitudeDiff * zoomSpeed;
 
             // Make sure the orthographic size never drops below zero.
-            thisCamera.orthographicSize = Mathf.Max(thisCamera.orthographicSize, targetSize);
+            thisCamera.orthographicSize = Mathf.Max(thisCamera.orthographicSize, targetSize - 1);
 
         }
 #endif
+
 #if UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetMouseButton(1))
+        {
+            if (firstPan)
+            {
+                lastPosition = Input.mousePosition;
+                firstPan = false;
+            }
+            lastTouch = Time.time;
+
+            Vector2 touchDeltaPosition = Input.mousePosition - lastPosition;
+            transform.Translate(-touchDeltaPosition.x * panRatio * panSpeed, -touchDeltaPosition.y * panRatio * panSpeed, 0);
+
+            lastPosition = Input.mousePosition;
+        }
+        else
+            firstPan = true;
+
         float deltaMagnitudeDiff = Input.mouseScrollDelta.y;
 
         // if the scroll wheel has been moved
         if (deltaMagnitudeDiff != 0)
         {
             // Store the last time zoom was requested
-            zoomTime = Time.time;
+            lastTouch = Time.time;
 
             // change the orthographic size based on the change scroll wheel position.
             thisCamera.orthographicSize += -deltaMagnitudeDiff * zoomSpeed;
 
             // Make sure the orthographic size never drops below zero.
-            thisCamera.orthographicSize = Mathf.Max(thisCamera.orthographicSize, targetSize);
+            thisCamera.orthographicSize = Mathf.Max(thisCamera.orthographicSize, targetSize - 1);
 
         }
 #endif
-        else if (thisCamera.orthographicSize > targetSize)
+
+        if (Time.time - lastTouch > returnDelay)
         {
-            if(Time.time - zoomTime > zoomDelay)
-                thisCamera.orthographicSize = Mathf.Clamp(Mathf.Lerp(thisCamera.orthographicSize, targetSize - 1f, zoomSpeed * Time.deltaTime), targetSize, Mathf.Infinity);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, cameraOffset, panSpeed * Time.deltaTime);
+            thisCamera.orthographicSize = Mathf.MoveTowards(thisCamera.orthographicSize, targetSize, zoomSpeed * Time.deltaTime);
         }
     }
 }
